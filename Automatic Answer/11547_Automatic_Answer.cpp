d49df85508aa5314bb68bddd7e2dca9d17c2c518
unknown <Fabho@FabianCalsina.masterline.com>
2015-04-24 19:56:41 -0400

    rm 3nn+1

---
+++ Automatic Answer/11547_Automatic_Answer.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int casos, n;
+
+    scanf("%d", &casos);
+
+    while(casos--)
+    {
+        scanf("%d", &n);
+        n *= 567;
+        n /= 9;
+        n += 7492;
+        n *= 235;
+        n /= 47;
+        n -= 498;
+
+        if(n <0)
+           n *= (-1);
+        n/=10;
+        printf("%d\n", n%10);
+
+    }
+return 0;
+}

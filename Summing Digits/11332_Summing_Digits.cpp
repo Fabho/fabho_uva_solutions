a6e93563070de3759795cb1075997012c996b35e
unknown <Fabho@FabianCalsina.masterline.com>
2015-04-24 18:57:24 -0400

    some probs

---
+++ Summing Digits/11332_Summing_Digits.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n)
+{
+    int tmp = 0;
+
+    while(n != 0)
+    {
+        tmp += (n%10);
+        n /= 10;
+    }
+
+    if(tmp < 10)
+      return tmp;
+    else
+        f(tmp);
+}
+
+int main()
+{
+    int n, ans;
+
+    while(scanf("%d", &n))
+    {
+        if(!n)
+          break;
+
+        ans = f(n);
+        printf("%d\n", ans);
+    }
+return 0;
+}

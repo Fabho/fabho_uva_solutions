089a49296fa6a09ccf9059484bbafaa49c920db8
unknown <Fabho@FabianCalsina.masterline.com>
2015-04-24 10:11:32 -0400

    Bafana Bafana

---
+++ Bafana Bafana/11805_Bafana_Bafana.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int casos, players, init, pases;
+
+    scanf("%d", &casos);
+
+    for(int i=1; i<=casos; i++)
+    {
+        scanf("%d %d %d", &players, &init, &pases);
+
+        for(int x=1; x<=pases; x++)
+        {
+            init++;
+
+            if(init > players)
+               init = 1;
+        }
+
+        printf("Case %d: %d\n", i, init);
+    }
+return 0;
+}

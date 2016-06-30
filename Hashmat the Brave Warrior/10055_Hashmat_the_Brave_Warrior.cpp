890f7580c48b599c98626da09df2c433237e7b2b
Fabho <fabian.calsina@gmail.com>
2016-06-30 16:32:08 -0400

    Solution

---
+++ Hashmat the Brave Warrior/10055_Hashmat_the_Brave_Warrior.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long a, b;
+
+    while(scanf("%ld %ld", &a, &b) != EOF)
+
+        printf("%ld\n", abs(a-b));
+
+return 0;
+}
